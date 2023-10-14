cf0578465195a3e0eb3c73feaa94074602dd007a
ankkarp <anna.a.k.2002@gmail.com>
2023-10-15 01:05:11 +0300

    added cmd args

--- test.py
+++ test.py
@@ -1,40 +1,44 @@
-import os
+import argparse
+
 from os import walk
-from model import NumberOcrModel
 import pandas as pd
 import csv
 
-MODEL_RESULT_PATH = 'results.csv'  # save path for csv with results
-# DATA_PATH = './test_images/'  # path to test images
-DATA_PATH = './test_images/'
+from model import NumberOcrModel
 
-images = []
-results = []
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i', '--input', help='Input image folder', type=str, default='test_images/full/images/')
+parser.add_argument('-o', '--output', help='Output csv-file', type=str, default='results.csv')
+parser.add_argument('-d', '--detection-weights', help='Path to YOLO detection model weights', type=str, default='models/best(6).pt')
 
 
-def to_csv(results):
-    with open(MODEL_RESULT_PATH, 'w', encoding='UTF8') as f:
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    images = []
+    results = []
+
+    model = NumberOcrModel(
+        detection_model=args.detection_weights,
+        rec_model='damo/cv_convnextTiny_ocr-recognition-general_damo',
+        angle_rec_model='Aster'
+    )
+
+    for (dirpath, dirnames, filenames) in walk(args.input):
+        images.extend(filenames)
+
+    for img in images:
+        res = model.predict(args.input + img)
+        if res:
+            results.append(res)
+
+    with open(args.output, 'w', encoding='UTF8') as f:
         fields = ('filename', 'type', 'number', 'is_correct')
         writer = csv.DictWriter(f, fieldnames=fields, lineterminator='\n')
         writer.writeheader()
         for res in results:
             writer.writerow(res[0])
-
-
-model = NumberOcrModel(
-    detection_model='./models/custom_yolov8x.pt',  # path to detection_model
-    rec_model='damo/cv_convnextTiny_ocr-recognition-general_damo',
-    angle_rec_model='Aster'
-)
-
-for (dirpath, dirnames, filenames) in walk(DATA_PATH):
-    images.extend(filenames)
-
-for img in images:
-    res = model.predict(DATA_PATH + img)
-    if res:
-        results.append(res)
-
-to_csv(results)
-file = pd.read_csv(MODEL_RESULT_PATH)
-print(file)
+    file = pd.read_csv(args.output)
+    print(file)
